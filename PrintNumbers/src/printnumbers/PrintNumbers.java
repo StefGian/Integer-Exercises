3430412295c0877e2380d85a631d1f7803cde1e0
Stefania Giannopoulou <stefania.gian91@gmail.com>
2019-08-17 16:21:09 +0300

    Divisible by 3, 5, and both
     A Java program to print numbers between 1 to 100 which are divisible by 3, 5 and by both.

---
+++ PrintNumbers/src/printnumbers/PrintNumbers.java
@@ -0,0 +1,36 @@
+package printnumbers;
+
+/**
+ *
+ * @author stefa
+ */
+public class PrintNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Divisible by 3:");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println("\n");
+        System.out.println("Divisible by 5:");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
+
+        System.out.println("\n");
+        System.out.println("Divisible by 3 and 5:");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+}
